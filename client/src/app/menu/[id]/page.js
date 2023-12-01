7ce2ad277bae547b546519cf3f703746688535a0
KateZzh <ekaterin.pro2@gmail.com>
2023-12-01 22:58:57 +0300

    id page: converted to server component

--- client/src/app/menu/[id]/page.js
+++ client/src/app/menu/[id]/page.js
@@ -1,31 +1,28 @@
-'use client';
-
 import style from './page.module.scss';
 import Image from 'next/image';
-import { useParams } from 'next/navigation';
 import { arrCardsMenu } from '@/storage/storageMenu';
-import { useState, useEffect } from 'react';
 
-export default function Id() {
-  const [card, setCard] = useState({});
-  const { id } = useParams();
+export default function Id({ params }) {
+  const { id } = params;
 
-  useEffect(() => {
-    setCard(arrCardsMenu.find((el) => el.id == id));
-  }, []);
+  const filteredCardsById = arrCardsMenu.find((el) => el.id == id);
 
   return (
     <>
       <div className={style.container}>
-        <Image className={style.img} alt={card.alt} src={card.imgForCard}></Image>
+        <Image
+          className={style.img}
+          alt={filteredCardsById.alt}
+          src={filteredCardsById.imgForCard}
+        ></Image>
 
         <div className={style.infoWrapper}>
-          <h2>{card.title}</h2>
-          <p className={style.description}>{card.description}</p>
+          <h2>{filteredCardsById.title}</h2>
+          <p className={style.description}>{filteredCardsById.description}</p>
 
           <div className={style.priceWrapper}>
             <p>Total:</p>
-            <p>{card.price}</p>
+            <p>{filteredCardsById.price}</p>
           </div>
         </div>
       </div>
